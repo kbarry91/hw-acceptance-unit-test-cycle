c5fba8823a36475f1fb8904d1f17e40391e6b1b2
kbarry91 <g00339811@gmit.ie>
2019-03-12 13:49:34 +0000

    [Part 2] Added path for Alies

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -15,12 +15,16 @@ module NavigationHelpers
 
     when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
 
+   
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-
+    
+    when /^the edit page for "(.*)"$/i # I go go to the edit page for "Alien"
+        edit_movie_path(Movie.find_by_title($1))
+        
     else
       begin
         page_name =~ /^the (.*) page$/
